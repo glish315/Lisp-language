a1e018f6c94dee34f6abddc6f9e3396f19dbc51c
glish315 <glish315@proton.me>
2025-02-02 23:38:11 +0100

    Portability fow Windows

--- main.c
+++ main.c
@@ -1,28 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#ifdef _WIN32
+#include <string.h>
+static char buffer[2048];
+
+char* readline(char* promt){
+    fputs(promt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = "\0";
+    return cpy;
+}
+
+void add_history(char* unused){}
+
+#else
 #include <readline/readline.h> //Readline library because on NixOS it is easy to download.
 #include <readline/history.h>
+#endif
 
-//static char input[2048]; Variable thats starts (once) at the start of the program, with file scope.
-
-                         
 int main(int argc, char** argv){
-
-    puts("https://github.com/glish315/Lisp-language");
-    puts("Press Ctrl+c to Exit");
+    puts("Lisp ver 0.0.0.0.1");
+    puts("Press Ctrl+c to Exit\n");
 
     while(1){
-//      fputs("Lispy> " ); stdout is "standart output stream", so it's deafult output, fputs work just like fprint 
-//      fgets(input, 2048, stdin); stdin is "standart input stream", read a line of input from stdin into "input" 
-
-        char* input = readline("Lispy> ");
-
+        char* input = readline("command");
         add_history(input);
 
-        printf("No you're a %s\n", input);
-
+        printf("Your command: %s\n", input);
         free(input);
+
     }
     return 0;
 }
+
